3447ee8308a5ca0e389f4f737b05db7edb1a3e08
Veeti <veeti.oliver@gmail.com>
2025-03-06 16:32:11 +0200

    jatketaan

--- game/script.js
+++ game/script.js
@@ -1,5 +1,8 @@
 let BG
 let robot
+let paddle_width = 80;
+let paddle_height = 30;
+
 function preload() {
     BG = loadImage('images/cavernous.png');
     robot = loadImage('images/robot.png');
@@ -13,4 +16,7 @@ canvas.parent('robopeli');
 function draw() {
     background('white')
     image(BG, 0, 0, windowWidth, windowWidth / 3)
+}
+function create_paddle(){
+  
 }
